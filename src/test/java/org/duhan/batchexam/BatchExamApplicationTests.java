13a29787358fbb61fd66c8dff9c3435e273cf79d
Duhanmo <devduhanmo@gmail.com>
2021-07-07 18:12:09 +0900

    first commit

---
+++ src/test/java/org/duhan/batchexam/BatchExamApplicationTests.java
@@ -0,0 +1,13 @@
+package org.duhan.batchexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
